f67b7ec5f2b42ded6042ad9361497e5eb1f703e7
aaronpope <aaronpope.dev@gmail.com>
2018-06-01 23:00:35 -0400

    Testing out imports in main script

--- secret_messages.py
+++ secret_messages.py
@@ -1,7 +1,9 @@
-import caesar
-import ciphers
+from affine import Affine
 
 if __name__ == '__main__':
     value = input("Enter something: ")
     print (value)
     print ("The project has started!")
+
+    a = Affine()
+    print(a.a, a.b)
