bc2d14b534b0ea88446a0f638f2367d80db63e12
aaronpope <aaronpope.dev@gmail.com>
2018-06-05 04:25:56 -0400

    Fixed to be inline with the intent of the cipher (only 26 characters)

--- atbash.py
+++ atbash.py
@@ -1,5 +1,17 @@
+import string
+
 from affine import Affine
 
 class Atbash(Affine):
     def __init__(self):
-        super().__init__(25, 25)
+        super().__init__(b=51)
+        self.CHARACTERS = string.ascii_uppercase
+        self.a = 51
+
+    # Atbash is only designed to function
+    def encrypt(self, text):
+        return super().encrypt(text.upper())
+
+
+    def decrypt(self, text):
+        return super().decrypt(text.upper())
