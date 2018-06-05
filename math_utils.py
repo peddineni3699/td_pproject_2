d052c57171571523191f9aaf348edc3ea5df1739
aaronpope <aaronpope.dev@gmail.com>
2018-06-05 04:43:35 -0400

    Refactored coprimes checker to better serve as a general utility

--- math_utils.py
+++ math_utils.py
@@ -9,12 +9,10 @@ def mult_mod_inv(inverse_target, mod):
     return factor
 
 
-# TODO: Could probably use a rename/refactor, since it's now a standalone utility
-def coprimes_identified(num1, num2):
+def are_coprimes(num1, num2):
     larger_number = num1 if num1 > num2 else num2
 
     for divisor in range(2, int(larger_number/2)):
         if num1 % divisor == 0 and num2 % divisor == 0:
-            raise ValueError("{} found to be a common divisor.\n'a' and {} must be coprimes.\nPlease enter a different value for 'a'."
-                .format(divisor, larger_number))
+            return divisor
     return False
