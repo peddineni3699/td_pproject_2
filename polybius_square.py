51a44a4d7ffc9960a140adca762bfaa818ff0627
aaronpope <aaronpope.dev@gmail.com>
2018-06-05 01:05:24 -0400

    Updated to use 100 characters

--- polybius_square.py
+++ polybius_square.py
@@ -2,15 +2,24 @@ import string
 
 from ciphers import Cipher
 
-# Utilizing letters and numbers 
-CHARACTERS = string.ascii_uppercase + string.digits + string.whitespace
+# In order to have square dimensions, 5 subsets of the string library  are utilized:
+#   Uppercase       26 characters
+#   Lowercase       26 characters
+#   Digits (0-9)    10 characters
+#   Punctuation     32 characters
+#   Whitespace       6 characters
+#
+# This allows the use of a 10x10 Polybius Square
+CHARACTERS = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation + string.whitespace
 
 class Polybius(Cipher):
     def __init__(self):
+        # This cipher does not require a key
         pass
 
     def encrypt(self, text):
         ciphertext = []
+        # Convert the text in put to uppercase
         text = text.upper()
         for char in text:
             try:
@@ -18,8 +27,8 @@ class Polybius(Cipher):
             except ValueError:
                 ciphertext.append(char)
             else:
-                digit1 = int(index / 6)
-                digit2 = index % 6
+                digit1 = int(index / 10)
+                digit2 = index % 10
                 ciphertext.append(str(digit1) + str(digit2))
         return " ".join(ciphertext)
 
@@ -29,7 +38,7 @@ class Polybius(Cipher):
         i_range = iter(range(0, len(ciphertext)))
         for i in i_range:
             try:
-                index = int(ciphertext[i]) * 6 + int(ciphertext[i+1])
+                index = int(ciphertext[i]) * 10 + int(ciphertext[i+1])
             except ValueError:
                 text.append(ciphertext[i])
             else:
