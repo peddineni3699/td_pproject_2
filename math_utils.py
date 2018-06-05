f86599357ea6c1aec6d419bab6ffb9c6286f7bd4
aaronpope <aaronpope.dev@gmail.com>
2018-06-05 03:05:35 -0400

    Added a utilities file for math functions

---
+++ math_utils.py
@@ -0,0 +1,19 @@
+def mult_mod_inv(inverse_target, mod):
+    # A rather brute-force method of finding the multiplicative modular inverse
+    factor = 1
+    mod_inv = inverse_target % mod
+
+    while (mod_inv != 1):
+        factor += 1 
+        mod_inv = inverse_target * factor % mod
+    return factor
+
+
+def coprimes_identified(num1, num2):
+    larger_number = num1 if num1 > num2 else num1
+
+    for divisor in range(2, int(larger_number/2)):
+        if num1 % divisor == 0 and num2 % divisor == 0:
+            raise ValueError("{} found to be a common divisor.\n'a' and {} must be coprimes.\nPlease enter a different value for 'a'."
+                .format(divisor, larger_number))
+    return False
