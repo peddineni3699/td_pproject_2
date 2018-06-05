d062c35751ff2b065360f8502dc8cf6ea7624ebf
aaronpope <aaronpope.dev@gmail.com>
2018-06-05 00:48:53 -0400

    Initial working state of Polybius Square

--- polybius_square.py
+++ polybius_square.py
@@ -3,19 +3,38 @@ import string
 from ciphers import Cipher
 
 # Utilizing letters and numbers 
-CHARACTERS = string.ascii_uppercase + string.digits
+CHARACTERS = string.ascii_uppercase + string.digits + string.whitespace
 
 class Polybius(Cipher):
     def __init__(self):
         pass
 
     def encrypt(self, text):
-        coded_text = []
+        ciphertext = []
+        text = text.upper()
         for char in text:
             try:
-                CHARACTERS.index(char)
+                index = CHARACTERS.index(char)
             except ValueError:
-                coded_text.append(char)
+                ciphertext.append(char)
+            else:
+                digit1 = int(index / 6)
+                digit2 = index % 6
+                ciphertext.append(str(digit1) + str(digit2))
+        return " ".join(ciphertext)
 
-    def decrypt(self, code):
-        pass
+    def decrypt(self, ciphertext):
+        ciphertext = ciphertext.replace(" ", "")
+        text = []
+        i_range = iter(range(0, len(ciphertext)))
+        for i in i_range:
+            try:
+                index = int(ciphertext[i]) * 6 + int(ciphertext[i+1])
+            except ValueError:
+                text.append(ciphertext[i])
+            else:
+                text.append(CHARACTERS[index])   
+                # Since the ciphertext is grouped into two letters,
+                #   a valid decoded character should advance the iterator by 2
+                next(i_range)    
+        return "".join(text)
