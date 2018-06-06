788b6d4bb87e2f663340e25551f9f90ad9d68121
aaronpope <aaronpope.dev@gmail.com>
2018-06-05 21:29:02 -0400

    Added script to clear the screen

--- secret_messages.py
+++ secret_messages.py
@@ -1,9 +1,15 @@
+import os
+
 from atbash import Atbash
 from affine import Affine
 from caesar import Caesar
 from polybius_square import Polybius
 
 if __name__ == '__main__':
+    def clear_screen():
+        os.system("cls" if os.name == "nt" else "clear")
+
+
     ciphers = {key: cipher_name for key, cipher_name in zip(range(1, 5), ["Affine", "Atbash", "Caesar", "Polybius Square"])}
     print ("Welcome to the Cipher Machine\n")
     print ("Let's start by selecting a cipher to use:")
