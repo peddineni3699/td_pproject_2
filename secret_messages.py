5930183ed45034b5e1b49dba955a338165be8946
aaronpope <aaronpope.dev@gmail.com>
2018-06-05 23:03:22 -0400

    started work on the UI/process flow

--- secret_messages.py
+++ secret_messages.py
@@ -6,25 +6,35 @@ from caesar import Caesar
 from polybius_square import Polybius
 
 if __name__ == '__main__':
+    ciphers = {key: cipher_name for key, cipher_name in 
+        zip(range(1, 5), ["Affine", "Atbash", "Caesar", "Polybius Square"])}
+
     def clear_screen():
         os.system("cls" if os.name == "nt" else "clear")
 
 
-    ciphers = {key: cipher_name for key, cipher_name in zip(range(1, 5), ["Affine", "Atbash", "Caesar", "Polybius Square"])}
-    print ("Welcome to the Cipher Machine\n")
-    print ("Let's start by selecting a cipher to use:")
-    for key, value in ciphers.items():
-        print("{}) {}".format(key, value))
-    cipher_choice = input ("SELECTION:  ")
+    def select_cipher():
+        while(True):        
+            for key, value in ciphers.items():
+                print("{}) {}".format(key, value))
+            
+            try:
+                cipher_choice = int(input ("\nEnter a number from the options above:  "))
+                if cipher_choice not in ciphers.keys():
+                    raise ValueError("Please select one of the available options.")
+            except ValueError:
+                clear_screen()
+                print ("Invalid entry.  Please select again.")
+            else:
+                break
+        return cipher_choice
 
-    try:
-        int(cipher_choice)
-    except ValueError:
-        # TODO: Implement this
-        print("BZZT!")
-    
+    # Begin UI flow
+    clear_screen()
+    print ("Welcome to the Cipher Machine!\n")
+    print ("Let's start by selecting a cipher to use:")
     while(True):
-        print ("The project has started!")
-        
+        selected_cipher = select_cipher()
+        print("You selected {}".format(ciphers[selected_cipher]))
         # TODO: Write the UI
         break
