67b8264b31f880347dde5fc17af6dcc76952325a
aaronpope <aaronpope.dev@gmail.com>
2018-06-09 12:21:55 -0400

    Added exit check at operation choice & cipher choice

--- secret_messages.py
+++ secret_messages.py
@@ -4,16 +4,25 @@ from atbash import Atbash
 from affine import Affine
 from caesar import Caesar
 from polybius_square import Polybius
+from sys import exit
 
 if __name__ == '__main__':
     ciphers = {key: cipher_name for key, cipher_name in 
         zip(range(1, 5), [Affine(), Atbash(), Caesar(), Polybius()])}
     OPERATION_ENCRYPT = "ENCRYPT"
     OPERATION_DECRYPT = "DECRYPT"
+    EXIT_ARGS = {"Q", "QUIT"}
 
 
     def clear_screen():
         os.system("cls" if os.name == "nt" else "clear")
+
+    def exit_check(choice):
+        if choice in EXIT_ARGS:
+            exit()
+
+    def print_invalid_entry_message():
+        print("Invalid entry.  Please select again.\n")
 
 
     def select_cipher():
@@ -21,13 +30,15 @@ if __name__ == '__main__':
             for key, value in ciphers.items():
                 print("{}) {}".format(key, value.__class__.__name__))
             
+            cipher_choice = input("\nEnter a number from the options above:  ")
+
             try:
-                cipher_choice = int(input ("\nEnter a number from the options above:  "))
-                if cipher_choice not in ciphers.keys():
-                    raise ValueError("Please select one of the available options.")
+                exit_check(cipher_choice.upper())
+                if int(cipher_choice) not in ciphers.keys():
+                    exit_check(cipher_choice)
+                    raise ValueError("Entered value does not exist in cipher list")
             except ValueError:
-                # clear_screen()
-                print ("Invalid entry.  Please select again.\n")
+                print_invalid_entry_message()
             else:
                 break
         return ciphers[cipher_choice]
@@ -36,19 +47,22 @@ if __name__ == '__main__':
         while(True):
             print ("Which operation would you like to perform?\nE) Encrypt\nD) Decrypt")
             try:
-                choice = input (">>>  ").upper()
-                if choice not in {"E", "D"}:
+                operation_choice = input (">>>  ").upper()
+                if operation_choice not in {"E", "D"}:
+                    exit_check(operation_choice)
                     raise ValueError('Enter "E" to encrypt or "D" to decrypt')
             except ValueError:
-                # clear_screen()
+                print_invalid_entry_message()
                 continue
             else:
-                return OPERATION_ENCRYPT if choice == "E" else OPERATION_DECRYPT
+                return OPERATION_ENCRYPT if operation_choice == "E" else OPERATION_DECRYPT
         
 
     # Begin UI flow
     clear_screen()
     print ("Welcome to the Cipher Machine!\n")
+    print ("Follow the prompts to encrypt or decrypt messages.")
+    print ("You can always enter 'Q' or 'quit' to leave the program.\n\n")
     while(True):
         selected_operation = select_encrypt_decrypt()
         print("\n{} from which cipher?".format(selected_operation))
