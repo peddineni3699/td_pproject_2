4cee372d718f0283a13faab61e36142a96076a6c
Aaron Pope <AaronPope@users.noreply.github.com>
2018-06-12 15:10:41 -0400

    Added comment for myself, re: rationale for file

---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+# Aaron Pope
+# 06/12/2018
+# Treehouse TechDegree - Python, Unit 2: Secret Messages
+
+# This file was added based on a preliminary understanding
+#   that it is required in order for this directory to be
+#   loaded correctly.
+#
+# If there is a better way to load the scripts from this
+#   subdirectory, I will implement it as soon as I 
+#   become aware of it!
