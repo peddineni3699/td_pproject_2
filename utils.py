8a2ac5658a163cd827ed35d18955b1c85c93dee8
aaronpope <aaronpope.dev@gmail.com>
2018-06-12 00:08:28 -0400

    initial commit; moved utility functionality to this file

---
+++ utils.py
@@ -0,0 +1,14 @@
+import os
+
+EXIT_ARGS = {"Q", "QUIT"}
+
+
+def exit_check(choice):
+    if choice in EXIT_ARGS:
+        exit()
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def print_invalid_entry_message():
+    print("Invalid entry.  Please select again.\n")
